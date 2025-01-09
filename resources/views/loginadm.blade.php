eb974214b93db9c1c4b39da360511570b5e090c5
AlcemirMacedo <alcemirmacedo@gmail.com>
2025-01-09 16:43:40 -0400

    formulário de login. fim de expediente 09/01

--- resources/views/loginadm.blade.php
+++ resources/views/loginadm.blade.php
@@ -6,9 +6,47 @@
 @section('content')
 
     <section class="card col-md-6">
+        <div class="row">
+            <div id="col1" class="col-md-6">
+                <figure>
+                    <img src="{{ asset('img/logo-w.png') }}" alt="">
+                </figure>
+                <h1>Painel Administrativo</h1>
+                <hr>
+                <h3>Todos os direitos reservados &copy</h3>
+            </div>
+            <div id="col2" class="col-md-6">
 
-            <h1>asdfas</h1>
+                <form action="" class="col-md-12">
+                    <h1>Login</h1>
+                    <hr>
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" class="form-control">
 
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
+                            <label for="senha">Senha</label>
+                            <input type="pass" name="senha" class="form-control">
+
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <button class="btn btn-success btn-block">Login</button>
+                    </div>
+
+                </form>
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="#">Criar uma conta</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
     <script src="{{ asset('js/jquery-3.4.0.min.js') }}"></script>
